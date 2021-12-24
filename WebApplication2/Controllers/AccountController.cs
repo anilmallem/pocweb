3c678665bc84dedb2be57cfb9b35988acebaa224
aninl <anil.mallem@gmail.com>
2021-12-24 15:23:30 +0530

    init commit

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using System.Web.Security;
+using System.Net;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private OfficeEntities db = new OfficeEntities();
+        // GET: Account
+      
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new OfficeEntities())
+            {
+                bool isValid = context.User.Any(x => x.Email == model.Email && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, false);
+                    return RedirectToAction("Index", "Users");
+                }
+                ModelState.AddModelError("", "Invalid Email and Password");
+                return View();
+            }
+           
+            
+        }
+       
+
+
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+
+            using (var context = new OfficeEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+                return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ WebApplication2/Models/Membership.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Membership
+    {
+        public int Id { get; set; }
+        public string OrganizationName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DOB { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
